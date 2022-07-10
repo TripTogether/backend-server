fb05b2d693fed363876f78df46d52437d33c39e4
minaamim <minaaa5814@gmail.com>
2022-07-10 16:40:48 +0900

    add: Set initial tasks
    - BaseEntity 생성
    - 에러 핸들링 코드 생성
    - 스웨거 초기 설정

---
+++ tripTogether/src/main/java/TT/tripTogether/TripTogetherApplication.java
@@ -0,0 +1,13 @@
+package TT.tripTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripTogetherApplication.class, args);
+	}
+
+}
---
+++ tripTogether/src/test/java/TT/tripTogether/TripTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package TT.tripTogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
